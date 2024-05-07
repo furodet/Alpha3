0016f5275656a7b014fa920c9bdcf3f8c25de5b9
David Furodet <david.furodet@gmail.com>
2024-05-07 19:42:57 +0200

    Envelopes: fixing and testing
    First experiments look good

--- record_outputs.m
+++ record_outputs.m
@@ -3,3 +3,5 @@ dco_out = out.yout.get("dco_out").Values.Data;
 audiowrite("out/dco_out.wav", dco_out, 44100);
 hp_out = out.yout.get("hp_out").Values.Data;
 audiowrite("out/hp_out.wav", hp_out, 44100);
+mix_out = out.yout.get("mix_out").Values.Data;
+audiowrite("out/mix_out.wav", mix_out, 44100);
