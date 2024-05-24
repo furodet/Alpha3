76c677b9ff666984d6690f706d790538951b1cb0
dfurodet <dfurodet@MATHWORKS-l7gTT>
2024-05-24 19:23:26 +0200

    Working on improving the attack/decay/release slopes

--- record_outputs.m
+++ record_outputs.m
@@ -1,7 +1,7 @@
 mkdir out;
 dco_out = out.yout.get("dco_out").Values.Data;
-audiowrite("out/dco_out.wav", dco_out, 44100);
+audiowrite("out/dco_out.wav", dco_out, 48000);
 hp_out = out.yout.get("hp_out").Values.Data;
-audiowrite("out/hp_out.wav", hp_out, 44100);
+audiowrite("out/hp_out.wav", hp_out, 48000);
 mix_out = out.yout.get("dca_out").Values.Data;
-audiowrite("out/dca_out.wav", mix_out, 44100);
+audiowrite("out/dca_out.wav", mix_out, 48000);
