8938953728434ce5259e327616cc3e9134819efb
David Furodet <david.furodet@gmail.com>
2024-06-02 18:07:08 +0200

    Start prototyping a LPF
    Using a simple biquad filter. Must plug controls to start tesing.

--- record_outputs.m
+++ record_outputs.m
@@ -1,7 +1,7 @@
 mkdir out;
 dco_out = out.yout.get("dco_out").Values.Data;
 audiowrite("out/dco_out.wav", dco_out, 48000);
-hp_out = out.yout.get("hp_out").Values.Data;
-audiowrite("out/hp_out.wav", hp_out, 48000);
+dcf_out = out.yout.get("dcf_out").Values.Data;
+audiowrite("out/dcf_out.wav", dcf_out, 48000);
 mix_out = out.yout.get("dca_out").Values.Data;
 audiowrite("out/dca_out.wav", mix_out, 48000);
